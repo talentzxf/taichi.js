ea0ce970f87b2de1343d1c64770f938ec5f1a737
talentzxf <talentzxf@hotmail.com>
2023-02-13 11:03:22 +0800

    Transparent looks better now

--- src/api/ui/SetImage.ts
+++ src/api/ui/SetImage.ts
@@ -24,7 +24,7 @@ class SetImage {
         this.renderFieldKernel = ti.classKernel(this,
             { image: ti.template() },
             (image: any) => {
-                ti.clearColor(this.renderTarget, [0.0, 0.0, 0.0, 1]);
+                ti.clearColor(this.renderTarget, [0.0, 0.0, 0.0, 0.0]);
                 for (let v of ti.inputVertices(this.VBO, this.IBO)) {
                     ti.outputPosition([v.x, v.y, 0.0, 1.0]);
                     ti.outputVertex(v);
@@ -34,7 +34,8 @@ class SetImage {
                     //@ts-ignore
                     let texelIndex = ti.i32(coord * (image.dimensions - 1))
                     let color = image[texelIndex].rgb
-                    ti.outputColor(this.renderTarget, color.concat([1.0]))
+                    // ti.outputColor(this.renderTarget, color.concat([1.0]))
+                    ti.outputColor(this.renderTarget, image[texelIndex])
                 }
             }
         )
